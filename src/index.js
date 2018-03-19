0c3cf91199f960067291634dd41682f2b22dc62e
Lajos.Meszaros <m_lajos@hotmail.com>
2018-03-19 16:20:33 +0100

    Created more helper functions; Moved primes variable into a function argument

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-const primes = [2, 3, 5]
-
 const canBeDividedWith = number => divisor => number % divisor === 0
 
-function findPrimes (currentNumber, to) {
+const last = array => array[array.length - 1]
+
+const endsWith3 = number => number.toString().endsWith('3')
+
+const incrementCandidate = number => number + (endsWith3(number) ? 4 : 2)
+
+function findPrimes (to, primes = [2, 3, 5]) {
+  let currentNumber = incrementCandidate(last(primes))
+
   while (currentNumber < to) {
     if (primes.find(canBeDividedWith(currentNumber)) === undefined) {
       primes.push(currentNumber)
     }
 
-    currentNumber += currentNumber.toString().endsWith('3') ? 4 : 2
+    currentNumber = incrementCandidate(currentNumber)
   }
+
+  return primes
 }
 
-findPrimes(7, 100)
+const primes = findPrimes(100)
 
 console.log(primes)
