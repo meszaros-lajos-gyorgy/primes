01f56d478d9a1e1148ef17d62f181c99f1ada48d
Lajos.Meszaros <m_lajos@hotmail.com>
2018-03-19 16:32:52 +0100

    Made findPrimes() immutable

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ const canBeDividedWith = number => divisor => number % divisor === 0
 
 const last = array => array[array.length - 1]
 
+const clone = array => array.slice(0)
+
 const endsWith3 = number => number.toString().endsWith('3')
 
 const incrementCandidate = number => number + (endsWith3(number) ? 4 : 2)
 
 function findPrimes (to, primes = [2, 3, 5]) {
-  let currentNumber = incrementCandidate(last(primes))
+  const foundPrimes = clone(primes)
+  let currentNumber = incrementCandidate(last(foundPrimes))
 
   while (currentNumber < to) {
-    if (primes.find(canBeDividedWith(currentNumber)) === undefined) {
-      primes.push(currentNumber)
+    if (foundPrimes.find(canBeDividedWith(currentNumber)) === undefined) {
+      foundPrimes.push(currentNumber)
     }
 
     currentNumber = incrementCandidate(currentNumber)
   }
 
-  return primes
+  return foundPrimes
 }
 
 const primes = findPrimes(100)
