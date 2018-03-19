79e56d2674f2f3a0ec9775960a244b8b7ad0fe3e
Lajos.Meszaros <m_lajos@hotmail.com>
2018-03-19 15:35:48 +0100

    Created canBeDividedWith()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const primes = []
 let currentNumber = 1
 
+const canBeDividedWith = (number, divisor) => number % divisor === 0
+
 function findPrimes () {
   const maxNumber = currentNumber + 10000
 
   while (currentNumber < maxNumber) {
     currentNumber += 2
 
-    const found = primes.find(prime => currentNumber % prime === 0)
+    const found = primes.find(prime => canBeDividedWith(currentNumber, prime))
 
     if (found === undefined) {
       primes.push(currentNumber)
