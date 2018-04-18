48b96d724bbae3af3672033aefbe68daaa243c92
Lajos.Meszaros <m_lajos@hotmail.com>
2018-04-19 00:56:38 +0200

    Added divisibility rule for 3 and added shortcut for case, when number is 0

--- src/divisibility.js
+++ src/divisibility.js
@@ -5,19 +5,42 @@ import {
 const isDivisibleBy = number => divisor => {
   let result
 
-  switch (divisor) {
-    case 2: {
-      const lastDigit = getLastDigit(number)
-      result = lastDigit === '0' || lastDigit === '2' || lastDigit === '4' || lastDigit === '6' || lastDigit === '8'
-    }
-      break
-    case 5: {
-      const lastDigit = getLastDigit(number)
-      result = lastDigit === '0' || lastDigit === '5'
-    }
-      break
-    default: {
-      result = number % divisor === 0
+  if (number === 0) {
+    result = true
+  } else {
+    switch (divisor) {
+      case 2: {
+        const lastDigit = getLastDigit(number)
+        result = lastDigit === '0' || lastDigit === '2' || lastDigit === '4' || lastDigit === '6' || lastDigit === '8'
+      }
+        break
+      case 3:
+        if (number.toString().length > 2) {
+          let sumOf147 = number.toString().match(/[147]+/g)
+          sumOf147 = sumOf147 === null ? 0 : sumOf147.join('').length
+
+          let sumOf258 = number.toString().match(/[258]+/g)
+          sumOf258 = sumOf258 === null ? 0 : sumOf258.join('').length
+
+          result = isDivisibleBy(sumOf147 - sumOf258)(3)
+        } else {
+          const multiplesUnder100 = [
+            3, 6, 9, 12, 15, 18, 21, 24, 27, 30,
+            33, 36, 39, 42, 45, 48, 51, 54, 57, 60,
+            63, 66, 69, 72, 75, 78, 81, 84, 87, 90,
+            93, 96, 99
+          ]
+          result = multiplesUnder100.includes(number)
+        }
+        break
+      case 5: {
+        const lastDigit = getLastDigit(number)
+        result = lastDigit === '0' || lastDigit === '5'
+      }
+        break
+      default: {
+        result = number % divisor === 0
+      }
     }
   }
 
